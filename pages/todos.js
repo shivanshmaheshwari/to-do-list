0a747119d18686222d162608cfa4f57e981acf98
Shivansh <shivanshmaheshwari99@gmail.com>
2023-05-23 02:47:33 +0530

    local storage connected

--- pages/todos.js
+++ pages/todos.js
@@ -1,15 +1,32 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
 
 const todos = () => {
-  const todos = [
-    { title: "q", desc: "w" },
-    { title: "q", desc: "w" },
-    { title: "weite", desc: "w\\r" },
-    { title: "12", desc: "23" },
-    { title: "34", desc: "45" },
-    { title: "rede", desc: "sdad" },
-    { title: "erqe", desc: "afwdfd" },
-  ];
+//   const todos = [
+//     { title: "q", desc: "w" },
+//     { title: "q", desc: "w" },
+//     { title: "weite", desc: "w\\r" },
+//     { title: "12", desc: "23" },
+//     { title: "34", desc: "45" },
+//     { title: "rede", desc: "sdad" },
+//     { title: "erqe", desc: "afwdfd" },
+//   ];
+const [todos, setTodos] = useState([])
+  useEffect(() => {
+    let todos = localStorage.getItem("todos")
+    setTodos(JSON.parse(todos))
+  
+  }, [])
+  
+
+  const deleteTodo = (title) => {  
+    let newTodos = todos.filter(item =>{
+        return item.title != title
+    })
+    localStorage.setItem("todos",JSON.stringify(newTodos))
+    setTodos(newTodos)
+  }
+
+  const editTodo = (first) => { second }
   return (
     <section class="text-gray-600 body-font">
       <div class="container px-5 py-24 mx-auto">
@@ -29,7 +46,7 @@ const todos = () => {
                   <img
                     alt="team"
                     class="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4"
-                    src="https://PICSUM.PHOTOS/2000/3000"
+                    src={`https://PICSUM.PHOTOS/600/900?a = ${item.title}`}
                   />
                   <div class="flex-grow sm:pl-8">
                     <h2 class="title-font font-medium text-lg text-gray-900">
@@ -37,7 +54,7 @@ const todos = () => {
                     </h2>
                     <p class="mb-4">{item.desc}</p>
                     <span class="inline-flex">
-                      <a class="text-gray-500">
+                      <a class="text-gray-500 cursor-pointer" onClick={() => {deleteTodo(item.title)}}>
                         {/* delete icon */}
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
@@ -49,28 +66,15 @@ const todos = () => {
                           <path d="M 14.984375 2.4863281 A 1.0001 1.0001 0 0 0 14 3.5 L 14 4 L 8.5 4 A 1.0001 1.0001 0 0 0 7.4863281 5 L 6 5 A 1.0001 1.0001 0 1 0 6 7 L 24 7 A 1.0001 1.0001 0 1 0 24 5 L 22.513672 5 A 1.0001 1.0001 0 0 0 21.5 4 L 16 4 L 16 3.5 A 1.0001 1.0001 0 0 0 14.984375 2.4863281 z M 6 9 L 7.7929688 24.234375 C 7.9109687 25.241375 8.7633438 26 9.7773438 26 L 20.222656 26 C 21.236656 26 22.088031 25.241375 22.207031 24.234375 L 24 9 L 6 9 z" />
                         </svg>
                       </a>
-                      <a class="ml-2 text-gray-500">
+                      <a class="ml-2 text-gray-500 cursor-pointer" onClick={editTodo}>
+                          {/* edit key */}
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
                           viewBox="0 0 50 50"
-                          width="50px"
-                          height="50px"
+                          width="30px"
+                          height="30px"
                         >
-                            {/* edit key */}
                           <path d="M 43.125 2 C 41.878906 2 40.636719 2.488281 39.6875 3.4375 L 38.875 4.25 L 45.75 11.125 C 45.746094 11.128906 46.5625 10.3125 46.5625 10.3125 C 48.464844 8.410156 48.460938 5.335938 46.5625 3.4375 C 45.609375 2.488281 44.371094 2 43.125 2 Z M 37.34375 6.03125 C 37.117188 6.0625 36.90625 6.175781 36.75 6.34375 L 4.3125 38.8125 C 4.183594 38.929688 4.085938 39.082031 4.03125 39.25 L 2.03125 46.75 C 1.941406 47.09375 2.042969 47.457031 2.292969 47.707031 C 2.542969 47.957031 2.90625 48.058594 3.25 47.96875 L 10.75 45.96875 C 10.917969 45.914063 11.070313 45.816406 11.1875 45.6875 L 43.65625 13.25 C 44.054688 12.863281 44.058594 12.226563 43.671875 11.828125 C 43.285156 11.429688 42.648438 11.425781 42.25 11.8125 L 9.96875 44.09375 L 5.90625 40.03125 L 38.1875 7.75 C 38.488281 7.460938 38.578125 7.011719 38.410156 6.628906 C 38.242188 6.246094 37.855469 6.007813 37.4375 6.03125 C 37.40625 6.03125 37.375 6.03125 37.34375 6.03125 Z" />
-                        </svg>
-                      </a>
-                      <a class="ml-2 text-gray-500">
-                        <svg
-                          fill="none"
-                          stroke="currentColor"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          class="w-5 h-5"
-                          viewBox="0 0 24 24"
-                        >
-                          <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
                         </svg>
                       </a>
                     </span>
