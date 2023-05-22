97273640b919eae3b5c31b86f8cae1d9b6eef765
Shivansh <shivanshmaheshwari99@gmail.com>
2023-05-23 01:47:44 +0530

    frontend complete

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import '@/styles/globals.css'
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header/>
+      <div className="container mx-auto min-h-screen">
+        <Component {...pageProps} />
+      </div>
+      <Footer/>
+    </>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,58 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import { useState } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+
+  const [todo, setTodo] = useState([title: "",desc: ""])
+
+  const addTodo = () => { 
+
+   }
+
+   const onchange = () => {  }
   return (
-    <div className='container'>
-      container
+    <div className="my-2  text-3xl">
+      
+      <section class="text-gray-600 body-font">
+        <div class="container px-5 py-24 mx-auto flex flex-wrap items-center">
+          {/* <h1 className="mb-1  ">ADD TODO</h1> */}
+          <div class="bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
+            <h2 class="text-gray-900 text-lg font-medium title-font mb-5">
+            ADD TODO
+            </h2>
+            <div class="relative mb-4">
+              <label for="full-name" class="leading-7 text-sm text-gray-600">
+                TUDO TITLE
+              </label>
+              <input
+                value={todo.title}
+                type="text"
+                id="full-name"
+                name="full-name"
+                class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              />
+            </div>
+            <div class="relative mb-4">
+              <label for="email" class="leading-7 text-sm text-gray-600">
+                TUDO TEXT
+              </label>
+              <input
+                value={todo.desc}
+                type="text"
+                id="email"
+                name="email"
+                class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              />
+            </div>
+            <button onClick={addTodo} class="text-white bg-indigo-500 w-2/12 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+              ADD TUDO
+            </button>
+          </div>
+        </div>
+      </section>
     </div>
-  )
+  );
 }
